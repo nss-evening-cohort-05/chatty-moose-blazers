aa7f0449c658735d2cb6f9e21f3d8eb60d8ec136
Heather Thacker <hhthacker@gmail.com>
2017-03-14 19:49:10 -0500

    created dom-handler file

---
+++ javascript/dom-handler.js
@@ -0,0 +1,7 @@
+// One IIFE should contain a function that accepts an element id, 
+// and the user message, 
+// and then add the user's message
+//  - along with the delete button - 
+//  to the specified parent element. 
+
+// makeDom(); write default JSON values to dom
