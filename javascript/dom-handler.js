5e60b0ce0737e8a74f5f8a05cc71e1fb14ff6836
Heather Thacker <hhthacker@gmail.com>
2017-03-14 20:23:40 -0500

    makeDom function outline

--- javascript/dom-handler.js
+++ javascript/dom-handler.js
@@ -1,7 +1,27 @@
-// One IIFE should contain a function that accepts an element id, 
-// and the user message, 
-// and then add the user's message
-//  - along with the delete button - 
-//  to the specified parent element. 
 
-// makeDom(); write default JSON values to dom
+var messageDiv = document.getElementById("user-message");
+var clearBoard = document.getElementById("buttonID");
+
+
+//grabs object message from private array by ID and adds delete button/HTML
+function makeDom(){
+	var messageString = "";
+	//object element ID + user message
+	// + delete button
+	messageDiv.innerHTML += messageString;
+};
+
+
+
+
+
+
+clearBoard.addEventListener("click", clearBoard(){
+
+	Chatty.clearMessage(message);
+	};
+);
+
+
+//listens for the enter key to add new message in DOM
+input.addEventListener("keyup", makeDom);
